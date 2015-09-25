9f2fe71b2a93ed6f025c19b59bf3374af942de04
Matt Ward <ward.matt@gmail.com>
2015-09-25 16:42:37 +0100

    Add TypeScript 1.6 specific code.
    abstract class support was added to TypeScript 1.6

---
+++ TypeScriptHTMLApp/abstractclasstest.ts
@@ -0,0 +1,12 @@
+﻿abstract class A {
+  foo(): number { return this.bar(); }
+  abstract bar(): number;
+}
+
+var a = new A();  // error, Cannot create an instance of the abstract class 'A'
+
+class B extends A {
+  bar() { return 1; }
+}
+
+var b = new B();  // success, all abstracts are defined
